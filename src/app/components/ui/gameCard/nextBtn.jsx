883be817e7c1fb2770a34a4174e5973f9ef83dfd
Heliya Rezaie <h.rezaie2479@gmail.com>
2025-08-03 14:50:35 +0330

    api

--- src/app/components/ui/gameCard/nextBtn.jsx
+++ src/app/components/ui/gameCard/nextBtn.jsx
@@ -2,9 +2,9 @@
 import { SVGNext, SVGReset } from '@/app/svg'
 import React from 'react'
 
-export default function NextBtn() {
+export default function NextBtn({onClick}) {
   return (
-    <button onClick={()=>console.log('hi')} className='w-[84px] h-[84px] rounded-full bg-white flex justify-center items-center p-[7px] shadow-2xl cursor-pointer'>
+    <button onClick={onClick} className='w-[84px] h-[84px] rounded-full bg-white flex justify-center items-center p-[7px] shadow-2xl cursor-pointer'>
         <SVGNext className='border-2 w-[70px] h-[70px] p-2 rounded-full flex justify-center items-center' />
     </button>
   )
--- src/app/components/ui/gameCard/resetBtn.jsx
+++ src/app/components/ui/gameCard/resetBtn.jsx
@@ -2,9 +2,9 @@
 import { SVGReset } from '@/app/svg'
 import React from 'react'
 
-export default function ResetBtn() {
+export default function ResetBtn({onClick}) {
   return (
-    <button onClick={()=>console.log('hi')} className='w-[45px] h-[45px] rounded-full bg-[#D52A2A] text-white flex justify-center items-center cursor-pointer'>
+    <button onClick={onClick} className='w-[45px] h-[45px] rounded-full bg-[#D52A2A] text-white flex justify-center items-center cursor-pointer'>
       <SVGReset />
     </button>
   )

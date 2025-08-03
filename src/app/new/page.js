a18e0f7a88698fe90353f2f0a8fb254395126021
mahla-mohamadi <mahlamhmd78@gmail.com>
2025-08-03 12:22:20 +0330

    manage-roles

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -8,13 +8,18 @@ export default function page() {
         console.log('Quantity changed to:', newQuantity);
     };
     return (
-        <div className="max-w-md mx-auto mt-10 bg-amber-600">
-                <Button label="توزیع نقش" onClick={() => console.log('Button clicked')} />
-                <Quantity 
-                    onQuantityChange={handleQuantityChange}
-                    className="my-4"
-                />
-                <Back link="../" />
+        <div className="container mx-auto px-2 lg:py-4">
+            <div className="max-w-[380px] w-full overflow-hidden mx-auto object-cover relative rounded-[24px] mainOverlay max-h-screen">
+                <img src="/img/domy.png" alt="" className="block w-full h-full"/>
+                <Back link="../" className="absolute right-[28px] top-[48px] z-20"/>
+                <div className="rounded-t-[25px] mainBox z-20">
+                    <h2 className="text-[20px] font-[800]">سناریو بازپرس</h2>
+                    <Quantity 
+                        onQuantityChange={handleQuantityChange}
+                    />
+                    <Button label="توزیع نقش" onClick={() => console.log('Button clicked')} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/svg.js
+++ src/app/svg.js
@@ -1,19 +1,67 @@
 export function SVGPlay(props){
     return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-play-icon lucide-play"
+        {...props}
+      >
+      <path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z" />
+    </svg>
+  );
+}
+export function SVGRightArrow(props){
+  return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
       fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="lucide lucide-play-icon lucide-play"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-    <path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z" />
-  </svg>
-);
+      <path d="M6.667 16h18.666M16 6.667 25.333 16 16 25.333" stroke="#000" />
+    </svg>
+  );
+}
+export function SVGMinus(props){
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="3 3 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M5 12h14"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+export function SVGPlus(props){
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="3 3 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M5 12h14m-7-7v14" stroke="#fff" strokeWidth={2} />
+    </svg>
+  );
 }

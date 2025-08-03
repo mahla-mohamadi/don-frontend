86f202ee751b77a5a24a32e9e45ade356a093f9d
Heliya Rezaie <h.rezaie2479@gmail.com>
2025-08-03 13:33:08 +0330

    start

--- src/app/components/ui/deck/rollHeader.jsx
+++ src/app/components/ui/deck/rollHeader.jsx
@@ -2,21 +2,18 @@ import React from 'react'
 
 export default function RollHeader() {
 
-     const roll ={
-            
+     const roll ={     
        id: 1,
        title : 'بازپرس',
        titleEng : 'INVESTIGATOR',
-       number : 'نقش 1 از 10',
-            
-           
+       number : 'نقش 1 از 10',     
      }
 
   return (
-   <div key={val.id}>
-       <h2>{val.title}</h2>
-       <h4>{val.titleEng}</h4>
-       <p>{val.number}</p>
+   <div key={roll.id} className='w-full text-center '>
+       <h2 className='text-[30px] font-extrabold mb-1 text-white'>{roll.title}</h2>
+       <h4 className='text-[12px] font-semibold mb-1 text-[#E6E6E6] tracking-1'>{roll.titleEng}</h4>
+       <p className='text-[10px] text-white'>{roll.number}</p>
    </div>
   
   )
--- src/app/components/ui/gameCard/nextBtn.jsx
+++ src/app/components/ui/gameCard/nextBtn.jsx
@@ -1,10 +1,11 @@
+'use client'
 import { SVGNext, SVGReset } from '@/app/svg'
 import React from 'react'
 
 export default function NextBtn() {
   return (
-    <button onClick={``} className='w-[84px] h-[84px] rounded-full bg-white flex justify-center items-center p-[7px] shadow-2xl'>
-        <SVGNext className='border w-[70px] h-[70px] p-2 rounded-full flex justify-center items-center' />
+    <button onClick={()=>console.log('hi')} className='w-[84px] h-[84px] rounded-full bg-white flex justify-center items-center p-[7px] shadow-2xl cursor-pointer'>
+        <SVGNext className='border-2 w-[70px] h-[70px] p-2 rounded-full flex justify-center items-center' />
     </button>
   )
 }

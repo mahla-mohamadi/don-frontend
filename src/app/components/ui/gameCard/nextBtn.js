b7312046e22349790074aebb00bb7ec689820d3c
mahla-mohamadi <mahlamhmd78@gmail.com>
2025-08-06 16:24:36 +0330

    Update nextBtn.js

--- src/app/components/ui/gameCard/nextBtn.js
+++ src/app/components/ui/gameCard/nextBtn.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function NextBtn({onClick}) {
   return (
-    <button onClick={onClick} className='animate-bounce animate-infinite animate-duration-[5000ms] animate-delay-[1ms] animate-ease-linear w-[84px] h-[84px] rounded-full bg-white flex justify-center items-center p-[7px] shadow-2xl cursor-pointer fixed left-0 right-0 bottom-[140px] m-auto'>
+    <button onClick={onClick} className='animate-bounce animate-infinite animate-duration-[5000ms] animate-delay-[1ms] animate-ease-linear w-[84px] h-[84px] rounded-full bg-white flex justify-center items-center p-[7px] shadow-2xl cursor-pointer absolute left-0 right-0 bottom-[140px] m-auto'>
         <SVGNext className='border-2 w-[70px] h-[70px] p-2 rounded-full flex justify-center items-center' />
     </button>
   )
